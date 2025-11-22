01fb3176cad7d5ce6711367ee669999af531d232
Teja3993 <karritej2@gmail.com>
2025-11-22 09:08:22 +0530

    AMZ-25 Implement cart state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// IMPORT CartProvider
+import { CartProvider } from "./context/CartContext.jsx";
+
+// WRAP App with CartProvider
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
